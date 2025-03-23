5b800358c9c0db8abb91b56d1faadec7e0c8f920
Tharunya S <tharunya865@gmail.com>
2025-03-23 19:18:35 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let dropdownMenu = document.querySelector(".dropdown-menu");
 
     dropdown.addEventListener("click", function (event) {
-        event.preventDefault(); // Prevents default behavior
-        event.stopPropagation(); // Prevents click from closing immediately
-        
-        // Toggle the "show" class on the dropdown menu
+        event.preventDefault(); 
+        event.stopPropagation(); 
         dropdownMenu.classList.toggle("show");
     });
-
-    // Close dropdown when clicking outside
     document.addEventListener("click", function (event) {
         if (!dropdown.contains(event.target)) {
             dropdownMenu.classList.remove("show");
